aae9f1280fc7b48a99c3c62c5cb2a3228458d6bc
hello665656765 <jeej6656@gmail.com>
2026-07-27 17:45:40 +0200

    Add CORS middleware to Express app

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const fetch = require('node-fetch');
 const fs = require('fs');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 // ============ CONFIG ============
